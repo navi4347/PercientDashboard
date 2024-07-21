7740da6d50c151d5f58ef426179311f8642fd2ea
navi4347 <naveenkonda.dev@gmail.com>
2024-07-22 01:23:14 +0530

    first commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,19 @@
 import LoginFom from "../Components/LoginFom";
 import Banner from "../Components/Banner";
 import HeaderTop from "../Components/HeaderTop";
+
 function Login() {
   return (
     <div>
       <HeaderTop />
-      <div className="Center Auth">
-        <div className="aleft">
-          <Banner />
-        </div>
-        <div className="container aright">
-          <LoginFom />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 d-none d-lg-block">
+            <Banner />
+          </div>
+          <div className="col-lg-6">
+            <LoginFom />
+          </div>
         </div>
       </div>
     </div>
